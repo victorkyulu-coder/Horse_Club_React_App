5ce420265c484efbe9493a1e1e83cfee0291a334
victorkyulu_coder <victorkyulu@gmail.com>
2025-11-14 01:37:01 +0300

    corrected syntaxx error

--- src/components/Reason.tsx
+++ src/components/Reason.tsx
@@ -1,5 +1,50 @@
-import 'styles.Reason.css';
-function reason() {
+import '../styles/Reason.css';
+function Reason() {
     <>
+     
+    <section className="reasons-section" id="reasons">
+      <div className="reason">
+        <h3 className="reason-title">expert technicians</h3>
+        <p className="reason-description">
+          Usage of the Internet is becoming more common due to rapid advancement
+          of technology and power.
+        </p>
+      </div>
+      <div className="reason">
+        <h3 className="reason-title">professional advice</h3>
+        <p className="reason-description">
+          Usage of the Internet is becoming more common due to rapid advancement
+          of technology and power.
+        </p>
+      </div>
+      <div className="reason">
+        <h3 className="reason-title">great support</h3>
+        <p className="reason-description">
+          Usage of the Internet is becoming more common due to rapid advancement
+          of technology and power.
+        </p>
+      </div>
+      <div className="reason">
+        <h3 className="reason-title">technical skills</h3>
+        <p className="reason-description">
+          Usage of the Internet is becoming more common due to rapid advancement
+          of technology and power.
+        </p>
+      </div>
+      <div className="reason">
+        <h3 className="reason-title">highly recommended</h3>
+        <p className="reason-description">
+          Usage of the Internet is becoming more common due to rapid advancement
+          of technology and power.
+        </p>
+      </div>
+      <div className="reason">
+        <h3 className="reason-title">positive reviews</h3>
+        <p className="reason-description">
+          Usage of the Internet is becoming more common due to rapid advancement
+          of technology and power.
+        </p>
+      </div>
+    </section>
     </>
 }
